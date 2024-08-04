dcbaddb9ea3b83f6800bd39725d4a637059469e8
REZASH600 <rezasharafdini973@gmail.com>
2024-08-04 11:10:47 +0330

    Refactor environment variable in local.py

--- multi_shop/config/settings/local.py
+++ multi_shop/config/settings/local.py
@@ -1,12 +1,20 @@
+import os
+
 from .base import *
 
-DEBUG = True
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-@$t6djrqsbbgg5em3gzp#j!uudo5*dw0t#amdm(7q#7j)#9##_')
 
-ALLOWED_HOSTS = []
+DEBUG = bool(os.environ.get("DEBUG", default=0))
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split()
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": path.join(BASE_DIR.parent, 'volumes', 'db', 'db.sqlite3'),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", path.join(BASE_DIR, 'volumes', 'db', 'db.sqlite3')),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
