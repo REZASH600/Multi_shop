34c6989413d779a0940000e699f48b6d5fe0648d
REZASH600 <rezasharafdini973@gmail.com>
2024-08-05 09:36:44 +0330

    Edit scripts.sh for new user model

--- multi_shop/scripts.sh
+++ multi_shop/scripts.sh
@@ -8,7 +8,7 @@ python manage.py migrate --noinput
 
 if [ ! -z "$DJANGO_SUPERUSER_USERNAME" ]; then
    echo "Creating superuser...";
-   python manage.py createsuperuser --noinput --username="$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" 2>/dev/null ;
+   python manage.py createsuperuser --noinput --phone "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" 2>/dev/null ;
    if [ $? -eq 0 ]; then
      echo "Superuser created successfully."
    else
