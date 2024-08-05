90d53eee829142f355e990bb9c6d83cae72b2a85
REZASH600 <rezasharafdini973@gmail.com>
2024-08-05 09:05:37 +0330

    Implement serializers, views, and URLs for User model

---
+++ multi_shop/apps/account/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.viewsets import ModelViewSet
+from apps.account import models
+from .serializers import *
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.parsers import MultiPartParser
+from rest_framework.permissions import IsAdminUser
+
+
+class UserView(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = models.User.objects.all()
+    authentication_classes = (JWTAuthentication,)
+    parser_classes = (MultiPartParser,)
+    permission_classes = (IsAdminUser,)
