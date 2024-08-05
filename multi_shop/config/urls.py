b9e3fa93d5fe4a10d5eb0145f402c4bfc35ca89c
REZASH600 <rezasharafdini973@gmail.com>
2024-08-05 08:00:37 +0330

    Install JWT package, setup JWT config, add JWT URLs, update requirements.txt

--- multi_shop/config/urls.py
+++ multi_shop/config/urls.py
@@ -18,11 +18,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
