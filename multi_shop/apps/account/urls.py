4fdd1650cd17eea2e693936b8cc5e233ad71928a
REZASH600 <rezasharafdini973@gmail.com>
2024-08-07 13:40:28 +0330

    Add logout view and  URL configuration

--- multi_shop/apps/account/urls.py
+++ multi_shop/apps/account/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'account_app'
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
