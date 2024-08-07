cc056f1f61504f03b67ce7348c790403eff64832
REZASH600 <rezasharafdini973@gmail.com>
2024-08-07 13:37:29 +0330

    Add login view, template, URL configuration, and install widget_tweaks package; update requirements

--- multi_shop/config/urls.py
+++ multi_shop/config/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('accounts/', include('apps.account.urls')),
 
     path('', include('apps.account.api.urls'))
 ]
