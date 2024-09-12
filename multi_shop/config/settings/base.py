68b0d7bfadb25e4d80c2e2580f527306e17fbcee
REZASH600 <rezasharafdini973@gmail.com>
2024-09-12 07:28:53 +0330

    Created Contact app

--- multi_shop/config/settings/base.py
+++ multi_shop/config/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "apps.account.apps.AccountConfig",
     "apps.product.apps.ProductConfig",
     "apps.cart.apps.CartConfig",
+    "apps.contact.apps.ContactConfig"
 ]
 
 MIDDLEWARE = [
